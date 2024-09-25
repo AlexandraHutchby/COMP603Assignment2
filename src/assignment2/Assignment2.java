3f682df0cc4e2437055fc75d7e094fc74a27a0d6
lainahende <lainahende01@gmail.com>
2024-09-26 10:08:58 +1200

    Test comment for github

--- src/assignment2/Assignment2.java
+++ src/assignment2/Assignment2.java
@@ -15,7 +15,7 @@ public class Assignment2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hello World!");
+        System.out.println("Hello World! This is a test");
     }
     
 }
