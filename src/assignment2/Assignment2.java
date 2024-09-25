336f4398a1bb934e6c5ed10734acf1072f5b13f1
AlexandraHutchby <alexandrahutchby@gmail.com>
2024-09-26 10:14:00 +1200

    testing

--- src/assignment2/Assignment2.java
+++ src/assignment2/Assignment2.java
@@ -16,6 +16,7 @@ public class Assignment2 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello World! This is a test");
+        System.out.println("hi");
     }
     
 }
